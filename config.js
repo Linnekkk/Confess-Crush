7574d13153deed1f2664542f6bf2a34fafa49fd1
linnekk <108207719+linnekk@users.noreply.github.com>
2022-06-26 11:58:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: " yêu tui thì bấm vào ii🥺",
+    introTitle: 'Đào Duy Vũ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    title: 'Purple is the last color of the rainbow🌈',
+    desc: 'Purple means I will trust and love you for a long time <3 ',
+    btnYes: 'yêu vkl <33',
+    btnNo: 'dỗi òi :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnReply: 'Gửi cho aiu <3',
+    reply: 'I purple you <3 and Đừng có dỗi em nữa mà :3',
+    mess: 'Pls em bỏ công ra làm để anh hớt giận đó🥺 ',
+    messDesc: 'Yêu em nhìu hơn nha🥺.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
