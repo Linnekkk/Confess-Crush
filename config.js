3e53fb934a859da3ceecdf37e579f917b6cd3c46
linnekk <108207719+linnekk@users.noreply.github.com>
2022-06-26 16:22:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     titleWeb: " iu tui thì bấm vô đây iii🥺🥺",
     introTitle: 'Đào Duy Vũ',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Purple is the last color of the rainbow🌈Purple means I will trust and love you for a long time😍',
-    desc: ' Purple means I will trust and love you for a long time😍 ',
+    introDesc: `Purple is the last color of the rainbow🌈
+    Purple means I will trust and love you for a long time😍`,
+    btnIntro: '^^hè hè hè ^^',
+    title: 'Đừng dỗi em nữa🥺🥺',
+    desc: ' iii mak 👉👈 ',
     btnYes: 'iu nhắmmm <33',
-    btnNo: 'còn dỗi vkl:( ',
+    btnNo: 'ko thích dỗi đấy làm sao:( ',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bae <3',
-    reply: ' I purple you <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    reply: 'Becaufe I purple you <33333333',
+    mess: 'em biết anh thương thương em mà nên đừng giận nữa nhá 😘😘',
     messDesc: 'thế đừng dỗi em nữa nhá pé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
